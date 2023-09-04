7b1fd9eeb88b260d39ae92b63e88ad5ca1ca4b63
tiamo <vk.tiamo@gmail.com>
2023-09-04 23:16:16 +0300

    fix: module name

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { registerPlugin } from '@capacitor/core';
 
 import type { SolanaMobileWalletAdapterModule } from './definitions';
 
-const SolanaMobileWalletAdapter = registerPlugin<SolanaMobileWalletAdapterModule>('SolanaMobileWalletAdapter', {
+const SolanaMobileWalletAdapter = registerPlugin<SolanaMobileWalletAdapterModule>('SolanaMobileWalletAdapterModule', {
   web: () => import('./web').then(m => new m.SolanaMobileWalletAdapterWeb()),
 });
 
