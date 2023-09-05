63adefe7ecff965db7e673b7c405bedbf4d83ae9
tiamo <vk.tiamo@gmail.com>
2023-09-06 00:52:25 +0300

    chore: cleanup

--- src/web.ts
+++ src/web.ts
@@ -1,29 +1,20 @@
 import { WebPlugin } from '@capacitor/core';
 import type { WalletAssociationConfig } from '@solana-mobile/mobile-wallet-adapter-protocol';
 
-import type {
-  SolanaMobileWalletAdapterModule,
-} from './definitions';
+import type { SolanaMobileWalletAdapterModule } from './definitions';
 
 export class SolanaMobileWalletAdapterWeb extends WebPlugin implements SolanaMobileWalletAdapterModule {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('[ExamplePlugin] ECHO', options);
-    return options;
-  }
-
   async startSession(config?: WalletAssociationConfig): Promise<void> {
-    console.log('[ExamplePlugin] startSession config: ', config)
-    // logic here
+    console.log('[SolanaMobileWalletAdapterWeb] startSession config: ', config)
   }
 
   async invoke(options: { method: string, params: any }): Promise<void> {
-    console.log('[ExamplePlugin] invoke method: ', options?.method)
-    console.log('[ExamplePlugin] invoke params: ', options?.params)
+    console.log('[SolanaMobileWalletAdapterWeb] invoke', options)
     // logic here
   }
 
   async endSession(): Promise<void> {
-    console.log('[ExamplePlugin] endSession')
+    console.log('[SolanaMobileWalletAdapterWeb] endSession')
     // logic here
   }
 }
